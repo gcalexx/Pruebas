e859da6bd6883a74b3a2828bf14e0c7d1a21225e
gcalexx <paco0cr7@gmail.com>
2018-10-28 01:34:34 -0500

    explorar

--- src/app/viewevents/viewevents.component.ts
+++ src/app/viewevents/viewevents.component.ts
@@ -26,74 +26,52 @@ export class VieweventsComponent implements OnInit {
 
     this.getEvents();
 
-    $(document).ready(function () {
+    $(document).ready(function(){
       $(".text, .text-user").hide();
       $(".nav").hide();
 
-      $("#menu, #menub").click(function () {
-        $("#mySidenav").css("width", "305px");
-        setTimeout(
-          function () {
-            $(".nav").show();
-          }, 300);
-        $("#menu, #menub").fadeOut(400);
-      });
-
-      $("#cerrar").click(function () {
-        $("#mySidenav").css("width", "0px");
-        $(".nav").hide();
-        $("#menu, #menub").fadeIn(400);
-      });
-
-      $("#menu-events").click(function () {
-        $("#esidenav").removeClass("hidden-xs");
+      $("#esidenav").mouseenter(function(){
         $("#esidenav").css("width", "230px");
-        setTimeout(function () {
+        setTimeout( function(){
           $(".text, .text-user").show();
         }, 185);
+        
       });
-
-      $("#close-events").click(function () {
-        $("#esidenav").css("width", "70px");
-        $("#esidenav").addClass("hidden-xs");
-      });
-
-      $("#esidenav").mouseenter(function () {
-        $("#esidenav").css("width", "230px");
-        setTimeout(function () {
-          $(".text, .text-user").show();
-        }, 185);
-
-      });
-      $("#esidenav").mouseleave(function () {
+      $("#esidenav").mouseleave(function(){
         $("#esidenav").css("width", "70px");
         $(".text, .text-user").hide();
       });
 
-      $("#usuario").click(function () {
+      $("#usuario").click(function(){
         $("#esidenav").css("width", "70px");
         $(".text, .text-user").hide();
       });
 
-      $("#explorar").click(function () {
+      $("#explorar").click(function(){
         $("#esidenav").css("width", "70px");
         $(".text, .text-user").hide();
       });
 
-      $("#crear").click(function () {
+      $("#crear").click(function(){
         $("#esidenav").css("width", "70px");
         $(".text, .text-user").hide();
       });
 
-      $("#buscar").click(function () {
+      $("#buscar").click(function(){
         $("#esidenav").css("width", "70px");
         $(".text, .text-user").hide();
       });
 
-      $("#configuracion").click(function () {
+      $("#configuracion").click(function(){
         $("#esidenav").css("width", "70px");
         $(".text, .text-user").hide();
       });
+
+      $("#ver-mas").click(function(){
+        $(".a-event").clone().appendTo("#row");
+        $("html").animate({ scrollTop: $(document).height() }, 1000);
+      });
+
     });
   }
 
