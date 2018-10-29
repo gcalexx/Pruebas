ec9e4cfb6ee7a4d8c617f78d44509447fc1d5e18
gcalexx <paco0cr7@gmail.com>
2018-10-28 21:42:11 -0600

    new event

--- src/app/newevent/newevent.component.ts
+++ src/app/newevent/newevent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-newevent',
@@ -10,6 +11,56 @@ export class NeweventComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  	$(document).ready(function(){
+  $(".text, .text-user").hide();
+  $(".nav").hide();
+
+  $("#esidenav").mouseenter(function(){
+    $("#esidenav").css("width", "230px");
+    setTimeout( function(){
+      $(".text, .text-user").show();
+    }, 185);
+    
+  });
+  $("#esidenav").mouseleave(function(){
+    $("#esidenav").css("width", "70px");
+    $(".text, .text-user").hide();
+  });
+
+  $("#usuario").click(function(){
+    $("#esidenav").css("width", "70px");
+    $(".text, .text-user").hide();
+  });
+
+  $("#explorar").click(function(){
+    $("#esidenav").css("width", "70px");
+    $(".text, .text-user").hide();
+  });
+
+  $("#crear").click(function(){
+    $("#esidenav").css("width", "70px");
+    $(".text, .text-user").hide();
+  });
+
+  $("#buscar").click(function(){
+    $("#esidenav").css("width", "70px");
+    $(".text, .text-user").hide();
+  });
+
+  $("#configuracion").click(function(){
+    $("#esidenav").css("width", "70px");
+    $(".text, .text-user").hide();
+  });
+
+  $("#ver-mas").click(function(){
+    $(".a-event").clone().appendTo("#row");
+    $("html").animate({ scrollTop: $(document).height() }, 1000);
+  });
+
+});
+
+
+
   }
 
 }
