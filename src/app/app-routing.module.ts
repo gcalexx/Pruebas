21efe9cf5414119ce178c646b08d3cd2d1a60ebf
Dory15 <dorianff3000@gmail.com>
2018-10-28 21:03:09 -0600

    Router newevents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { VieweventsComponent } from './viewevents/viewevents.component';
+import { NeweventComponent } from './newevent/newevent.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'viewevents', component: VieweventsComponent}
+  {path: 'viewevents', component: VieweventsComponent},
+  {path: 'newevent', component: NeweventComponent}
 ];
 
 @NgModule({
