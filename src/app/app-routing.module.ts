21375e17e7c9893939cc5c4dda01ab3e35eba8d0
Dory15 <dorianff3000@gmail.com>
2018-11-02 22:06:35 -0600

    Routeo de viewevent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { noAuth } from './services/noAuth.guard';
 
 import { HomeComponent } from './home/home.component';
 import { VieweventsComponent } from './viewevents/viewevents.component';
+import { VieweventComponent } from './viewevent/viewevent.component';
 import { NeweventComponent } from './newevent/newevent.component';
 import { LogoutComponent } from './logout/logout.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, canActivate: [noAuth]},
   {path: 'viewevents', component: VieweventsComponent, canActivate: [Auth]},
+  {path: 'viewevent', component: VieweventComponent, canActivate: [Auth]},
   {path: 'newevent', component: NeweventComponent, canActivate: [Auth]},
   {path: 'logout', component: LogoutComponent, canActivate: [Auth]},
   {path: '**', component: HomeComponent, canActivate: [noAuth]}
